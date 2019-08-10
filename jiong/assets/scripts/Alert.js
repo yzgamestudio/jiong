19f037cb3895d6efb38387c9c6b09262e46a2fe2
周明 <mmix1009@163.com>
2019-08-10 15:43:15 +0800

    [feature] 实现关卡切换

--- jiong/assets/scripts/Alert.js
+++ jiong/assets/scripts/Alert.js
@@ -12,31 +12,36 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        nextLevel: {
+            default: null,
+            type: cc.Node,
+        },
         cotent: ''
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+    onLoad () {
+        //监听触摸开始事件
+        this.nextLevel.on(cc.Node.EventType.TOUCH_START, function (t) {
+            //函数体内写事件发生时的事情
+            //当触摸开始是打印以下字样
+            console.log("触摸开始");
+            this.jumpNextScene();
+        }, this);
+        //监听作用域内触摸抬起事件
+        this.nextLevel.on(cc.Node.EventType.TOUCH_ENDED, function (t) {
+            console.log("触碰结束");
+        }, this);
+    },
 
     start () {
 
     },
+
+    jumpNextScene () {
+        cc.director.loadScene("level2");
+    }
 
     // update (dt) {},
 });
